22a4d930be46cd89b04da633fe9b15dc14302a01
j0x0 <jlundst@gmail.com>
2025-02-21 18:03:58 +0100

    grouping sprites

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     time = pygame.time.Clock()
-    myPlayer = Player( (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player( (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        myPlayer.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        myPlayer.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = (time.tick(60))/1000
 
